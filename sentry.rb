e7e7d250b38c1284edcb1b844003106e16535a39
deploy <deploy@domU-12-31-39-0F-44-23.compute-1.internal>
2010-04-13 12:53:21 -0700

    completed refactor to module

--- sentry.rb
+++ sentry.rb
@@ -33,16 +33,50 @@ module Sentry
     end
 
     def process
-      `echo 'eystuff #{@request.pretty_inspect} ' | nc localhost 5678`
+      build_task_list(Sentry::rules['processes'])
+      log "Processing request #{ARGV.collect {|a| a.inspect}.join(' ')}"
+      log @task_list_description
+      @task_list.each do |t|
+        do_task t
+      end
+      log "Done."
     end
 
     private
 
-    def build_task_list
-      
+    def build_task_list(rules)
+      @task_list = nil
+      path_index = 0
+      path = [:platform,:process_type,:condition]
+      path.inject(rules) do |r,p|
+        @task_list = r["__default"] if r.has_key? "__default"
+        rc = (r.has_key?(@request[p])) ? r[@request[p]] : nil
+        break if rc.nil?
+        path_index += 1
+        @task_list = rc unless rc.nil? or path_index < path.length
+        rc
+      end
+
+      description_elements = path.collect {|p| "#{p.to_s} '#{@request[p]}'"}
+      @task_list_description = if @task_list.nil?
+        @task_list = []
+	"Nothing to do"
+      elsif path_index == 0
+        "Running default tasks -- no higher-level matching tasks found for #{description_elements.join(', ')}"
+      elsif path_index < path.length
+        "Running default tasks for #{description_elements[0...path_index].join(', ')} - could not find higher-level match for #{description_elements[path_index..-1].join(', ')}"
+      else
+        "Running tasks for #{description_elements.join(', ')}"
+      end
     end
 
-    def dotask
+    def do_task(task)
+      log "  - running '#{task.respond_to?(:has_key?) ? task.keys[0].to_s + "(options: " + task.values[0].inspect + ")" : task.to_s}"
+    end
+
+    def log(msg)
+      msg.gsub!(/'/,"'\"'\"'")
+      `echo '[SENTRY] #{msg}' | nc localhost 5678`
     end
 
   end
@@ -52,39 +86,5 @@ task = Sentry::Task.new(*ARGV)
 task.process()
 
 __END__
-# Find what to do
-to_do_options = [
-  rules['processes'].fetch(platform,{}).fetch(process_name,{}).fetch(condition,nil),
-  rules['processes'].fetch(platform,{}).fetch(process_name,{}).fetch('__default',nil),
-  rules['processes'].fetch(platform,{}).fetch('__default',nil)
-]
-
-puts "Processing request #{ARGV.inspect}:"
-unless to_do_options[0].nil?
-  puts "  Processing actions for #{condition} condition:"
-  print "   - running '#{to_do_options[0].collect {|x| x.respond_to?(:has_key?) ? x.keys[0].to_s + "(options: " + x.values[0].inspect + ")" : x.to_s}.join("'\n   - running '")}'\n"
-  puts "Done.\n"
-  exit
-end
-
-unless to_do_options[1].nil?
-  puts "  Could not find handler for #{condition} condition, running default #{process_name} handler:"
-  print "   - running '#{to_do_options[1].collect {|x| x.respond_to?(:has_key?) ? x.keys[0].to_s + "(options: " + x.values[0].inspect + ")" : x.to_s}.join("'\n   - running '")}'\n"
-  puts "Done.\n"
-  exit
-end
-
-unless to_do_options[2].nil?
-  puts "  Could not find handler for #{condition} or default conditions for #{process_name}, running default handler for #{platform}:"
-  print "   - running '#{to_do_options[2].collect {|x| x.respond_to?(:has_key?) ? x.keys[0].to_s + "(options: " + x.values[0].inspect + ")" : x.to_s}.join("'\n   - running '")}'\n"
-  puts "Done.\n"
-  exit
-end
-
-if to_do_options == [nil,nil,nil]
-  puts "Nothing to do\n"
-  exit
-end
-
 
 
